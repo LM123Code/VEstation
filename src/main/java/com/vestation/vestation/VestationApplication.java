ee191b173223a531cf8135d4d1332b3a08ed8426
Negotiation <1648014891@qq.com>
2020-06-27 10:10:29 +0800

    项目环境的初始化

---
+++ src/main/java/com/vestation/vestation/VestationApplication.java
@@ -0,0 +1,13 @@
+package com.vestation.vestation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VestationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VestationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/vestation/vestation/VestationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vestation.vestation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VestationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
