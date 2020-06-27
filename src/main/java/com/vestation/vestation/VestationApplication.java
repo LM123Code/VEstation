e589c7752ae2bb920993cbad88ac62dc9063db6c
LM123Code <1456466514@qq.com>
2020-06-27 10:31:05 +0800

    创建develop分支

--- src/main/java/com/vestation/vestation/VestationApplication.java
+++ src/main/java/com/vestation/vestation/VestationApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class VestationApplication {
 
+    
     public static void main(String[] args) {
         SpringApplication.run(VestationApplication.class, args);
     }
