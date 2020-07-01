9cfcf82c02dfb0b068d0e2ddbb46c94850bdcdbb
LM123Code <1456466514@qq.com>
2020-07-01 18:58:37 +0800

    1、在yml文件中添加数据库其他配置、mybatispuls其他配置 2、在resources文件下创建mybatis存放mybatis其他相关配置信息，如驼峰命名配置等 3、将mapper的xml文件移到resources文件夹下新建的mapper文件夹下，便于管理 4、编写登录接口VxUserController：login（），根据openid插入用户，如果用户存在，更新头像url和昵称，如果用户不存在则插入新用户

--- src/main/java/com/zhiyi/vestation/controller/VxUserController.java
+++ src/main/java/com/zhiyi/vestation/controller/VxUserController.java
@@ -1,9 +1,13 @@
 package com.zhiyi.vestation.controller;
 
 
+import com.zhiyi.vestation.service.VxUserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 /**
  * <p>
@@ -16,6 +20,23 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/vestation/vx-user")
 public class VxUserController {
+
+    @Autowired
+    VxUserService vxUserService;//注入微信用户的service对象
+
+    /**
+     * 登录方法
+     *
+     * @param appid 小程序唯一标识
+     * @param secret
+     * @param js_code 登陆凭证
+     * @param userAvatarUrl 头像地址
+     * @param nickName 昵称
+     * @return
+     */
+    public Map<String, String> login(String appid, String secret,String js_code, String userAvatarUrl, String nickName){
+        return vxUserService.login(appid, secret, js_code, userAvatarUrl, nickName);
+    }
 
 }
 
--- src/main/java/com/zhiyi/vestation/service/impl/VxUserServiceImpl.java
+++ src/main/java/com/zhiyi/vestation/service/impl/VxUserServiceImpl.java
@@ -4,7 +4,13 @@ import com.zhiyi.vestation.pojo.VxUser;
 import com.zhiyi.vestation.mapper.VxUserMapper;
 import com.zhiyi.vestation.service.VxUserService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.zhiyi.vestation.utils.HttpRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import net.sf.json.JSONObject;
 import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * <p>
@@ -17,4 +23,51 @@ import org.springframework.stereotype.Service;
 @Service
 public class VxUserServiceImpl extends ServiceImpl<VxUserMapper, VxUser> implements VxUserService {
 
+    @Autowired
+    VxUserMapper vxUserMapper;//注入为微信user的mapper类
+
+    /**
+     * 登录方法
+     *
+     * @param appid 小程序唯一标识
+     * @param secret
+     * @param js_code 登陆凭证
+     * @param userAvatarUrl 头像地址
+     * @param nickName 昵称
+     * @return
+     */
+    @Override
+    public Map<String, String> login(String appid, String secret, String js_code, String userAvatarUrl, String nickName) {
+        Map map = new HashMap<>();
+        //登录凭证不能为空
+        if (js_code == null || js_code.length() == 0) {
+            map.put("status", 0);
+            map.put("msg", "code 不能为空");
+            return map;
+        }
+        //小程序唯一标识   (在微信小程序管理后台获取)
+        String wxspAppid = appid;
+        //小程序的 app secret (在微信小程序管理后台获取)
+        String wxspSecret = secret;
+        //////////////// 1、向微信服务器 使用登录凭证 code 获取 session_key 和 openid ////////////////
+        //请求参数：唯一标识+秘钥+登录凭证
+        String params = "appid=" + wxspAppid + "&secret=" + wxspSecret + "&js_code=" + js_code ;
+        //发送请求
+        String sr = HttpRequest.sendGet("https://api.weixin.qq.com/sns/jscode2session", params);
+        //解析相应内容（转换成json对象）
+        JSONObject json = JSONObject.fromObject(sr);
+        //获取会话密钥（session_key）
+        String session_key = json.get("session_key").toString();
+        //用户的微信唯一标识（openid）
+        String openid = (String) json.get("openid");
+
+//        插入或更新用户
+        int status = vxUserMapper.insertOrUpdateByOpenid(openid, nickName, userAvatarUrl);
+
+
+        map.put("status", status); //登录成功status为1，失败为0
+        map.put("openid",openid);
+
+        return map;
+    }
 }
