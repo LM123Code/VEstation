021b6af35a7ac72d62952b4a5f54d61d4567d63f
Negotiation <1648014891@qq.com>
2020-08-02 11:19:09 +0800

    解决ResultStatus报错问题

--- src/main/java/com/zhiyi/vestation/pojo/ResultStatus.java
+++ src/main/java/com/zhiyi/vestation/pojo/ResultStatus.java
@@ -7,26 +7,33 @@ import lombok.Data;
 
 
 @Data
-public class ResultStatus<T> {
+public class ResultStatus {
     private String code;
     private String msg;
-    private T data;
-    JSONObject resultStatus = new JSONObject();
-    public ResultStatus(String code, String msg, T data) {
-        this.setCode(code);
-        this.setMsg(msg);
-        this.setData(data);
-    }
-    public ResultStatus(String code,String msg) {
-        this.setCode(code);
-        this.setMsg(msg);
-    }
+    private Object data;
+
     public String toString() {
+        JSONObject resultStatus = new JSONObject();
         resultStatus.put("code",this.code);
         resultStatus.put("msg",this.msg);
         resultStatus.put("data",this.data);
-        return this.resultStatus.toJSONString();
+        return resultStatus.toJSONString();
     }
+
+    public ResultStatus setCode(String code){
+        this.code = code;
+        return this;
+    }
+
+    public ResultStatus setMsg(String msg){
+        this.msg = msg;
+        return this;
+    }
+    public ResultStatus setData(Object data){
+        this.data = data;
+        return this;
+    }
+
 
 
 }
